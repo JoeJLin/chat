a21c18cd8d56d0c3d8c08cce4fb3b44d36685e98
Joe Lin <JOE@Joes-MacBook-Pro.local>
2018-07-24 09:41:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var socket = require('socket.io');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+var server = app.listen(3000, function(){
+  console.log('listening on port 3000')
+})
+
+//setup socket
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('connected socket');
+  console.log(socket.id)
+
+  //handle chat event
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data)
+    console.log(data);
+  });
+
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+    console.log(data)
+  })
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/chat.js
@@ -0,0 +1,32 @@
+//make connection
+var socket = io.connect('http://localhost:3000');
+
+var message = document.getElementById('message');
+var output = document.getElementById('output');
+var button = document.getElementById('send');
+var username = document.getElementById('username');
+var feedback = document.getElementById('feedback');
+
+
+//emit event
+//pass chat to app.js and let app.js send it to all other clients
+button.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        username: username.value
+    });
+})
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', username.value);
+})
+
+//listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.username + '</strong>' + ': ' + data.message + '</p>';
+})
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...' + '</em></p>';
+})
