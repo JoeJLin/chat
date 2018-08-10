27ab477b3a437d2e43e3fbb40646e707195919da
Joe Lin <JOE@Joes-MacBook-Pro.local>
2018-08-09 17:52:35 -0700

    testing heroku

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
   saveUninitialized: true
 }));
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   console.log('listening on port 3000')
 })
 
